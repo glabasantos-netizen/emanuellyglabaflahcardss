c79d08928903a2e8a36ef07e96f195ce360e1f30
glabasantos-netizen <glaba.santos@escola.pr.gov.br>
2025-08-05 10:51:01 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Francês',
+    'Como se diz 'livro' em francês?',
+    'Livre'
+)
+
+criaCartao(
+    'Física',
+    'Oque é a Velocidade média?',
+    'A velocidade de um corpo é a distância que ele percorre na unidade e tempo'
+)
+
+criaCartao(
+    'Jogos esportivos',
+    'Qual é a aultura da rede e vôlei no jogo masculino e femininos?',
+    '2,43 m e 2,24 m'
+)
+
+criaCartao(
+    'Geografia',
+    'qual país é transcontinental?',
+    'Rússia'
+)
